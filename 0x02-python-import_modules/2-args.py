00fceaa30746842631b3c4344aff39831f7cfb2c
Sergio <2016@holbertonschool.com>
2020-12-10 23:05:22 -0500

    Add file 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
 counter = 0
 for iter in sys.argv[1:]:
@@ -8,6 +10,3 @@ counter2 = 0
 for iter2 in sys.argv[1:]:
     counter2 += 1
     print('{}: {}'.format(counter2, iter2))
-
-if __name__ == "__main__":
-    import sys
