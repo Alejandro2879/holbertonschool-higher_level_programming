580004389bb36e1d73dba3e6abcbd93488434504
Sergio <2016@holbertonschool.com>
2020-12-15 19:05:46 -0500

    Add files README.md 0-main.py 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    iter = 0
+    while iter < len(my_list):
+        print("{}".format(my_list[iter]))
+        iter += 1
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Directory contain functions to practice with some data structures -lists and tuples- in Python
