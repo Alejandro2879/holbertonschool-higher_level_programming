6fe7ef6a77b81dd7eb58308e3724f5c314aa0445
Sergio <2016@holbertonschool.com>
2020-12-15 19:29:32 -0500

    Add file 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    iter = 0
-    while iter < len(my_list):
-        print("{0:d}".format(my_list[iter]))
-        iter += 1
+    for iter in my_list:
+        print("{:d}".format(iter))
