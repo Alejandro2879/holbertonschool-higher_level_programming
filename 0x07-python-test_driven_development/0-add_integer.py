32dea209e28cb2d35d7315fb8961d32ffc1588c1
Sergio <2016@holbertonschool.com>
2021-01-08 23:44:00 -0500

    Add files 0-main.py 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Mudule define function add_integer that add 2 numbers
+"""
+
+def add_integer(a, b=98):
+    """ Funcion add a and b
+
+    Args:
+        a (int): [Firts number]
+        b (int): [Second number]. Defaults to 98.
+
+    Raises:
+        TypeError: [If a is not an int or a float]
+        TypeError: [If b is not an int or float]
+
+    Returns:
+        [int]: [Return a + b and cast the result to int]
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
