6b500c5168f14e3446c542eb282227887f363075
Sergio <2016@holbertonschool.com>
2021-01-18 20:46:19 -0500

    Add files 13-is_palindrome.c 13-main.c lists.h

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a linked list is palindrome.
+ * @head: Pointer to the head of the list
+ * Return: 0 if it's palindrome, 0 if is't.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp =  *head;
+	int counter, iter;
+	int buffer[2048];
+
+	if (!head)
+		return (0);
+	if (!*head || !(*head)->next)
+		return (1);
+	for (counter = 0; tmp; counter++)
+	{
+		buffer[counter] = tmp->n;
+		tmp = tmp->next;
+		printf("%d\n", counter);
+	}
+
+	for (iter = 0; counter > iter; iter++, counter--)
+	{
+		if (buffer[counter - 1] != buffer[iter])
+			return (0);
+	}
+	return (1);
+}
