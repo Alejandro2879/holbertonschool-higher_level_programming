a6729d031bd9b1767c63712ca0293b9e267b32b8
Sergio <2016@holbertonschool.com>
2021-01-19 17:11:54 -0500

    Add documentation and fix indentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""[Module define a lookup function that return all attributes of obj.]
+"""
+
+
 def lookup(obj):
-    print(list(obj.__dict__))
+    """[Function return all available attributes and methods of the superclass]
+
+    Args:
+        obj ([class]): [Parent class]
+    """
+    return(list(obj.__dict__))
