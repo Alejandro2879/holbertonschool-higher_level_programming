1b4375179e94f630c76c517c19f6b58f50435c0a
Sergio <2016@holbertonschool.com>
2021-01-20 23:16:41 -0500

    Add files 6-main.py 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""[Module define the load_from_json_file function]
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """[Function create an object from a json file]
+
+    Args:
+        filename (file): [Json file]
+
+    Returns:
+        [Obj]: [Python representation of the json file]
+    """
+    with open(filename, 'r', encoding="utf-8") as json_file:
+        obj = json.load(json_file)
+        return json.dumps(obj)
