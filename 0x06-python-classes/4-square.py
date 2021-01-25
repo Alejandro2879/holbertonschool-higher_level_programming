e0e9feec3ea973400da7def123275fbcc4bffc04
Sergio <2016@holbertonschool.com>
2021-01-24 23:33:01 -0500

    Add files 4-main.py 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Class square"""
+
+
+class Square:
+    """[Class square]
+    """
+
+    def __init_(self, size=0):
+        self.size = size
+
+        @property
+        """[Getter of size]
+        """
+        def size(self):
+            return self.__size
+
+        """[Setter of size]
+        """
+        @size.setter
+        def size(self, size):
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+        def area(self):
+            """[Return the area of the square]
+            """
+            return self.__size ** 2
