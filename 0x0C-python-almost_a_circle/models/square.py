b94f3535977a12a205940dc1329178949a5bc439
Sergio <2016@holbertonschool.com>
2021-01-27 14:18:38 -0500

    Fix documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 from models.rectangle import Rectangle
-"""[]
+"""[Module define the Square class]
 """
 
 
 class Square(Rectangle):
-        """[]
+        """[Square class]
         """
         def __init__(self, size, x=0, y=0, id=None):
+                """[Define the __ini__ method]
+                """
                 super().__init__(size, size, x, y, id)
 
-        """[]
-        """
         @property
+        """[Getter of size]
+        """
         def size(self):
                 return self.width
 
         @size.setter
         def size(self, size):
+                """[Setter of size]
+                """
                 if not isinstance(size, int):
                         raise TypeError("width must be an integer")
                 elif size <= 0:
@@ -25,18 +29,18 @@ class Square(Rectangle):
                 self.width = size
                 self.height = size
 
-        """[]
-        """
         def __str__(self):
+                """[Print square with the '#' character]
+                """
                 return '[{}] ({}) {}/{} - {}'.format(Square.__name__,
                                                         self.id,
                                                         self.x,
                                                         self.y,
                                                         self.width)
 
-        """[]
-        """
         def update(self, *args, **kwargs):
+                """[Update the attributes values]
+                """
                 lis_attr = ['id', 'size', 'x', 'y']
                 if args and args[0] is not None:
                         for value in range(len(args)):
@@ -45,7 +49,7 @@ class Square(Rectangle):
                         for key in kwargs:
                                 setattr(self, key, kwargs[key])
 
-        """[]
-        """
         def to_dictionary(self):
+                """[Return dic]
+                """
                 return self.__dict__
