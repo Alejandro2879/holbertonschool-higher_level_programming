5344d5f17badc73ea6db3bbb7ec5de88f201d93d
Sergio <2016@holbertonschool.com>
2021-02-06 12:29:00 -0500

    Add file 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+
+ops = ['+', '-', '*', '/']
+first_e = int(sys.argv[1])
+second_e = int(sys.argv[3])
+operator = sys.argv[2]
+result = 0
+
+if len(sys.argv) is not 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+if operator not in ops:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+for i in sys.argv[1:]:
+    if operator is '+':
+        result = add(first_e, second_e)
+    elif operator is '-':
+        result = sub(first_e, second_e)
+    elif operator is '*':
+        result = mul(first_e, second_e)
+    elif operator is '/':
+        result = div(first_e, second_e)
+
+print("{} {} {} = {}".format(first_e, operator, second_e, result))
