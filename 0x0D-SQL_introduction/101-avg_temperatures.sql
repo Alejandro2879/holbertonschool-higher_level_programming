45b12956d5d4f7929a19a66091b3ba681be36bd9
Sergio <2016@holbertonschool.com>
2021-03-02 13:31:37 -0500

    Add file 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Script import data from a database
-SELECT `city`, AVG(`value`) AS av_temp
+SELECT city, AVG(value) AS 'av_temp'
 FROM temperatures
-GROUP BY `city`
-ORDER BY `av_temp` DESC;
+GROUP BY city
+ORDER BY av_temp DESC;
