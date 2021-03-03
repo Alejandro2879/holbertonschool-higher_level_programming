efdfaccbd1c0e0606655b10b206c99acd3599797
Sergio <2016@holbertonschool.com>
2021-03-03 00:05:25 -0500

    Change name of the second column to: number_of_shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,10 +6,10 @@
 -- 5) Results must be sorted in descending order by the number of shows linked
 -- 6) You can use only one SELECT statement
 -- 7) The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name AS genre, COUNT(tv_shows.genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show.genres.genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_shows_genres
-ON tv_genres.id = tv_shows.genres.genre_id
-GROUP BY tv_shows.genres.genre_id
+ON tv_genres.id = tv_shows_genres.genre_id
+GROUP BY tv_shows_genres.genre_id
 ORDER BY number_of_shows DESC;
 
