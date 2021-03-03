a6218462d64b561153a6c6fb2a0d2f9299b0db21
Sergio <2016@holbertonschool.com>
2021-03-03 00:09:41 -0500

    Delete spaces around =

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,7 +9,6 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_shows_genres
-ON tv_genres.id = tv_shows_genres.genre_id
+ON tv_genres.id=tv_shows_genres.genre_id
 GROUP BY tv_shows_genres.genre_id
 ORDER BY number_of_shows DESC;
-
