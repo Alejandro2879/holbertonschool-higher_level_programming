6aaa4746f2b0ec1441705ea6ada518aabdcede54
Sergio <2016@holbertonschool.com>
2021-03-23 00:04:01 -0500

    Add file 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,16 @@
 let major = parseInt(process.argv[2]);
 let secMajor = parseInt(process.argv[2]);
 
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(0);
+}
+
 function secondMajor () {
+  if (isNaN(major)) {
+    console.log(0);
+  }
   for (let iter = 0; iter < process.argv.length; iter++) {
     if (process.argv[iter] > major) {
       secMajor = major;
