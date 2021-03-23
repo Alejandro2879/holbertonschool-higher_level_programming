0509d5a9c6d1c51b697a405b4b7d4050fd6297cc
Sergio <2016@holbertonschool.com>
2021-03-22 23:04:35 -0500

    Add file 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,6 @@ if (isNaN(nTimes)) {
   console.log('Missing size');
 } else {
   for (let fIter = 0; fIter < nTimes; fIter++) {
-    for (let sIter = 0; sIter < nTimes; sIter++) {
-      process.stdout.write('x');
-    }
-    console.log('');
+    console.log('X'.repeat(nTimes));
   }
 }
