45a009c54be72a27bbb227e840260d7167c1abb7
Sergio <2016@holbertonschool.com>
2021-03-24 01:03:02 -0500

    Add files 5-main.js 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+
+  print () {
+    for (let i = 0; i < this.size; i++) {
+      console.log('X'.repeat(this.size));
+    }
+  }
+
+  double () {
+    this.size = this.size * 2;
+  }
+};
