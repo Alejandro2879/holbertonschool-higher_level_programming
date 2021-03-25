a00a81647aeb0f01ed16a4a61256497c0c8734e0
Sergio <2016@holbertonschool.com>
2021-03-24 19:22:04 -0500

    AFix super()

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -6,14 +6,4 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-
-  print () {
-    for (let i = 0; i < this.size; i++) {
-      console.log('X'.repeat(this.size));
-    }
-  }
-
-  double () {
-    this.size = this.size * 2;
-  }
 };
