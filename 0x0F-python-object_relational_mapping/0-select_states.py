bd2b55ba9838e8774086b3f6c4ad9dc49d367fb8
Sergio <2016@holbertonschool.com>
2021-03-26 00:18:39 -0500

    Add close() to clode the connection

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,22 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
+db = MySQLdb.connect(
+  host='localhost',
+  port=3306,
+  user=sys.argv[1],
+  passwd=sys.argv[2],
+  db=sys.argv[3]
+)
 
-c.execute("SELECT * FROM states")
+cursor = db.cursor()
 
-rows = c.fetchall()
+cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+rows = cursor.fetchall()
 
 for eachRow in rows:
     print(eachRow)
+
+cursor.close()
+db.close()
