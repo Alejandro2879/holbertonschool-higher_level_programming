eaf2501531897f11822edd90db8f6a576cfc3183
Sergio <2016@holbertonschool.com>
2021-03-28 15:38:58 -0500

    Put imports out of if statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
       port=3306,
       user=sys.argv[1],
       passwd=sys.argv[2],
-      db=sys.argv[3]
+      db=sys.argv[3],
     )
 
     cursor = db.cursor()
 
     cursor.execute(
-      "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-      )
+      "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    )
 
     rows = cursor.fetchall()
 
