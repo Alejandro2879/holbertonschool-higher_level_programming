468964d7a734ea174030dbf484e2dc508df1bd65
Sergio <2016@holbertonschool.com>
2021-04-01 19:16:14 -0500

    Add if __name__...

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,13 +7,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-  sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '___main__':
 
-Session = sessionmaker()
-session = Session(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+      sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-new_name = session.query(State).filter(State.id == 2).first()
-new_name.name = "New Mexico"
-session.commit()
-session.close()
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    new_name = session.query(State).filter(State.id == 2).first()
+    new_name.name = "New Mexico"
+    session.commit()
+    session.close()
