ad5be184ec493413025fc2bb3e8880b65e91ae34
Sergio <2016@holbertonschool.com>
2021-04-09 20:42:54 -0500

    Add file 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                                                
 # Script that takes in a URL as an argument, sends a GET request to the URL.                                               
-curl "$1" -s -H "X-HolbertonSchool-User-Id: 98"
+curl $1 -s -H "X-HolbertonSchool-User-Id: 98"
