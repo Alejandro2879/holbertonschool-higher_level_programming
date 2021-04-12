6ddafe6e3223d3cfa998ca530aab3339820ada84
Sergio <2016@holbertonschool.com>
2021-04-12 11:50:00 -0500

    Remake the code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,11 @@
 """ Script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
+if __name__ == '__main__':
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-    print(resp.read())
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        response = resp.read()
+        print("- Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode()))
