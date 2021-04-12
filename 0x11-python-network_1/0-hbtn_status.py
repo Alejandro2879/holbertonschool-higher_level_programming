60dafae58b41ca726cad0f161a06c9415f10a2b8
Sergio <2016@holbertonschool.com>
2021-04-12 12:00:36 -0500

    Delete '-' before 'Body response:'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ import urllib.request
 
 if __name__ == '__main__':
 
-    with urllib.request.urlopen('http://0.0.0.0:5050/status') as resp:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         response = resp.read()
-        print("- Body response:")
+        print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
         print("\t- utf8 content: {}".format(response.decode("UTF-8")))
