5949ee9823b4709ca369f16cdaa85ef20715dc16
Sergio <2016@holbertonschool.com>
2021-04-12 20:06:50 -0500

    Delete varaible url

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,8 @@ Script that takes in a URL sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header
 """
 
-import sys
 import requests
+import sys
 
 
 if __name__ == '__main__':
